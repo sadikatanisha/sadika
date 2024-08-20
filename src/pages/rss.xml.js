dd7fc253eed11c974a97ee797091243cd262275d
sadikatanisha <sadikatanisha@gmail.com>
2024-08-20 15:48:18 +0600

    portfolio update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,11 +2,11 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function get() {
-  const posts = await getCollection('posts');
+  const posts = await getCollection("posts");
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
-    site: 'https://my-blog-site.netlify.app',
+    title: "Astro Learner | Blog",
+    description: "My journey learning Astro",
+    site: "https://my-blog-site.netlify.app",
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
